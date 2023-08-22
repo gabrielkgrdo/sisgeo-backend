08d98ee024cab6844a78d7ec82110d574af13e69
gabrielkgrdo <gabrielderoque@gmail.com>
2023-08-22 11:06:54 -0300

    deploy

--- src/main/java/com/kleyber/SISGEO/SisgeoApplication.java
+++ src/main/java/com/kleyber/SISGEO/SisgeoApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SisgeoApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SisgeoApplication.class, args);
 	}
+	
+	public String PORT = System.getenv("PORT");
 
 }

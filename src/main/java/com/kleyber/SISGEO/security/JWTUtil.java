5c54077267f8cbc647f1033a1f142afa0782c397
gabrielkgrdo <gabrielderoque@gmail.com>
2023-08-28 13:45:11 -0300

    alteração token

--- src/main/java/com/kleyber/SISGEO/security/JWTUtil.java
+++ src/main/java/com/kleyber/SISGEO/security/JWTUtil.java
@@ -1,7 +1,6 @@
 package com.kleyber.SISGEO.security;
 
 import java.util.Date;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,32 +11,29 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JWTUtil {
-
+	
 	@Value("${jwt.expiration}")
 	private Long expiration;
-
+	
 	@Value("${jwt.secret}")
 	private String secret;
-
+	
 	public String gerarToken(String email) {
-		return Jwts.builder().setSubject(email).setExpiration(new Date(System.currentTimeMillis() + expiration))
-				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
+		return Jwts.builder()
+				.setSubject(email)
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
 	}
 
 	public boolean tokenValido(String token) {
 		Claims reivindicacoes = getReividicacoes(token);
-		if (reivindicacoes != null) {
+		if(reivindicacoes != null) { 
 			String username = reivindicacoes.getSubject();
 			Date expirationDate = reivindicacoes.getExpiration();
-			Date now = new Date();
-
-			// Define o TimeZone para o Horário de Brasília (GMT-3)
-            TimeZone brasiliaTimeZone = TimeZone.getTimeZone("GMT-3");
-
-            // Ajusta a data de expiração para o Horário de Brasília
-            expirationDate.setTime(expirationDate.getTime() - brasiliaTimeZone.getRawOffset());
-
-			if (username != null && expirationDate != null && now.before(expirationDate)) {
+			Date now = new Date(System.currentTimeMillis());
+			
+			if(username != null && expirationDate != null && now.before(expirationDate)) {
 				return true;
 			}
 		}
@@ -47,15 +43,15 @@ public class JWTUtil {
 	private Claims getReividicacoes(String token) {
 		try {
 			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
-		} catch (Exception e) {
+			} catch (Exception e) { 
 			return null;
 		}
-
+		
 	}
 
 	public String getUsername(String token) {
 		Claims reivindicacoes = getReividicacoes(token);
-		if (reivindicacoes != null) {
+		if(reivindicacoes != null) {
 			return reivindicacoes.getSubject();
 		}
 		return null;
